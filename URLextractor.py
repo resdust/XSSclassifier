560d6f8732e66af6ee758f234b1918dd2c4b849e
resdust <resdust_li@163.com>
2019-04-12 21:24:16 +0800

    URL feature extractor version 1.0 and its result(limited to 10

--- URLextractor.py
+++ URLextractor.py
@@ -1,28 +1,32 @@
-#!python3
+#!/usr/bin/python3
 # -*- encoding:utf-8 -*-
 
-import re
+import re, csv, time, socket
 import pandas as pd
+import urllib.error as error
 import urllib.request
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
+#open file and load data(websites with XSSed tags) to returned value
 def opendata(path):
     df = pd.read_csv(path)
     list_label = df.columns.values 
     list_data =df.values.tolist()
-    print(list_label)
+    print("These columns have been read:",list_label) #print columns header
     return list_data
 
+#extract relative urls from website
 def extract(url):
-    meg = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
-    r = urllib.request.Request(url,headers=meg)
-    fp = urllib.request.urlopen(r)
+    meg = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'} #fake header
+    r = urllib.request.Request(url,headers=meg) 
+    fp = urllib.request.urlopen(r) #send request and open url   
     s = fp.read()
     soup = BeautifulSoup(s,features="html.parser")
-    polist = soup.find_all('a')
-    urlDct = {}
-    for tag in polist:
+    alist = soup.find_all('a') #all the 'a' tags
+    fp.close()
+    urlDct = {} #extracted urls as key while occurence number as value
+    for tag in alist:
         href = tag.get('href')
         if href != None:
             if(href in urlDct):
@@ -31,32 +35,78 @@ def extract(url):
                 urlDct[href] = 1
     return urlDct
 
-def URLfeature(dct):
-    #domains = []
-    longUrlCount = maxUrlSize = maxOcurDomain = obfuscatedCharCount = 0
-    for d in dct:
+#extract features from urls in unique website
+def URLfeature(suburl):
+    if(suburl==None):
+        return (['Failed to open the website!','' ,'' ,'' ])
+    longUrlCount = maxUrlSize = xssCount = obfuscatedCharCount = 0
+    XSS = [r"<scrip",r"</script",r"<iframe",r"</ifirame",r"response_write(",r"eval(",r"prompt(",r"alert(",r"javascript:",r"document.cookie"]
+    first = 0
+    for d in suburl:
+        size = len(d)
+        #The maximum size of URLs
+        if (first == 0):
+            first = 1
+            maxUrlSize = size
+        else:
+            maxUrlSize = (size > maxUrlSize) and size or maxUrlSize
+        
+        #Total count of URLs with high level malicious XSS keywords
+        for key in XSS:
+            if (d.find(key) != -1):
+                xssCount += 1
+                break
+
+        #Total count of long URLs
         if len(d) > 120:
             longUrlCount += 1
-    print('longUrlCount=',longUrlCount)
+       
+        #Total count of URLs with maximum number of obfuscated characters
+        special = re.split(r'%', d)
+        if (len(special)-1) > (len(d)/3):
+            obfuscatedCharCount += 1
+    return [longUrlCount, maxUrlSize, xssCount, obfuscatedCharCount]
 
-def domain(url):
-    res = urlparse(url).netloc
-    print(res)
-    return res
-
-url1 = 'https://www.jianshu.com/p/1476a181fc57'
-
+#write analysed result to a new csv file
+def loadData(website, xss, data):
+    with open("Features.csv","a",newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([website, xss, data[0], data[1], data[2], data[3]])
+       
 def start():
+    stime = time.process_time()
+    socket.setdefaulttimeout(20)
+    try:
+        with open("Features.csv","w+",newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Website','XSSed', 'longUrlCount', 'maxUrlSize', 'xssCount', 'obfuscatedCharCount'])   
+    except PermissionError as e:
+        print("Failed to open file! Please close your \'Features.csv\' file.")
+        exit(0)
     path = r'.\XSSArchive3.csv'
-    LIST = opendata(path)
+    LIST = opendata(path) #read data
     limit = 0
     for url in LIST:
-        url[1] = (url[1]=='R') and 1 or 0
-        subUrl = extract('http://'+url[0])
-        URLfeature(subUrl)
         limit += 1
-        if(limit >= 1):
+        try:
+            subUrl = extract('http://'+url[0]) 
+        except error.URLError as e:
+            subUrl = None
+            print("Unable to open:", url[0])
+            print(e.reason)
+        except WindowsError as e:
+            subUrl = None
+            print("Unable to open:", url[0])
+            print(e)
+        else:
+            ttime = time.process_time()
+            print("Loading website", limit, "...", url[0], "...at time:", ttime-stime)
+        time.sleep(10)
+        features = URLfeature(subUrl)
+        xss = (url[1]=='R') and 1 or 0 #1 for XSSed and 0 for non-XSSed
+        loadData(url[0], xss, features)
+        if(limit >= 10):
             break
+    print("END")
 
-subUrl = extract(url1)
-URLfeature(subUrl)
+start()
